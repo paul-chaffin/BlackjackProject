07fe7b02dd1b694344a6133ab829adcfb03fc543
paul-chaffin <pechaffin@pm.me>
2022-04-08 16:10:45 -0400

    app save

--- src/com/skilldistillery/blackjack/BlackjackApplication.java
+++ src/com/skilldistillery/blackjack/BlackjackApplication.java
@@ -4,7 +4,18 @@ public class BlackjackApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		BlackjackApplication app = new BlackjackApplication();
+		
+		app.run();
+	}
+	
+	public void run() {
+		//TODO this begins the game
+		displayMenu();
+	}
+	
+	public void displayMenu() {
+		//TODO 
 	}
 
 }
