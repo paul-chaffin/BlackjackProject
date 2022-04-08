ab43ed5efb87a1d3ffa3ff679aaa5acb1b233a15
paul-chaffin <pechaffin@pm.me>
2022-04-08 18:43:18 -0400

    fixed dealer.showHand() so blind isn't visible until appropriate

--- src/com/skilldistillery/blackjack/BlackjackApplication.java
+++ src/com/skilldistillery/blackjack/BlackjackApplication.java
@@ -1,8 +1,14 @@
 package com.skilldistillery.blackjack;
 
-import com.skilldistillery.blackjack.cards.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import com.skilldistillery.blackjack.cards.Dealer;
+import com.skilldistillery.blackjack.cards.Deck;
+import com.skilldistillery.blackjack.cards.Player;
 
 public class BlackjackApplication {
+	private Scanner kb = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -20,20 +26,42 @@ public class BlackjackApplication {
 
 		showCurrentStanding(d, p);
 
-		displayMenu();
 	}
 
 	public void showCurrentStanding(Dealer d, Player p) {
 		System.out.print("Dealer shows:\n\t");
-		System.out.print(d.showHand() + " || (blind)\n");
-		System.out.println("\tHand value: " + d.showHand().getValue() + " (+ blind)");
+		System.out.println(d.showHand());
+		System.out.println("\tHand value: " + d.getHandValueNoBlind() + " (+ blind)");
+//		System.out.println(d.getHandSize());
 		System.out.print("You show:\n\t");
 		System.out.print(p.showHand());
 		System.out.print("\n\tHand value: " + p.getHandValue());
 	}
 
-	public void displayMenu() {
-		// TODO
+	public void displayMenu(Deck deck, Dealer d, Player p) {
+		String choice = "";
+		System.out.println("(S)tand or (H)it?");
+		do {
+			try {
+				choice = kb.next().toUpperCase();
+				switch (choice) {
+				case H:
+					d.dealCardToPlayer(deck, p);
+					break;
+				case S:
+					break;
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("Please enter S to Stand or H to Hit.");
+			}
+		} while (choice != "S");
+
+		dealerTurn(deck, d, p);
 	}
 
+	public void dealerTurn(Deck deck, Dealer d, Player p) {
+		do {
+
+		} while (d.getHandValueWithBlind() < 17);
+	}
 }
--- src/com/skilldistillery/blackjack/cards/Dealer.java
+++ src/com/skilldistillery/blackjack/cards/Dealer.java
@@ -14,16 +14,45 @@ public class Dealer {
 		}
 	}
 
+	public void dealCardToPlayer(Deck deck, Player p) {
+		p.takeCardFromDealer(deck.dealCard());
+	}
+
+	public void dealCardToSelf(Deck deck) {
+
+	}
+
 	public int getHandSize() {
 		return hand.cards.size();
 	}
-	
-	public Card showHand() {
-		return hand.cards.get(0);
+
+	public StringBuilder showHand() {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < (hand.cards.size() - 1); i++) {
+			sb.append(hand.cards.get(i).toString() + " || ");
+		}
+		sb.append("BLIND");
+		return sb;
 	}
-	
+
 	public Card showBlind() {
-		return hand.cards.get(1);
+		return hand.cards.get(-1);
+	}
+
+	public int getHandValueWithBlind() {
+		int value = 0;
+		for (Card c : hand.cards) {
+			value += c.getValue();
+		}
+		return value;
+	}
+
+	public int getHandValueNoBlind() {
+		int value = 0;
+		for (int i = 0; i < (hand.cards.size() - 1); i++) {
+			value += hand.cards.get(i).getValue();
+		}
+		return value;
 	}
 
 }
